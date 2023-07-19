8df6d2321bfe81aafb02b5358626a9fe8ac5bae6
EdwardFine <edfine03@hotmail.com>
2023-07-19 12:03:46 -0700

    Email

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -52,6 +52,10 @@ public class HomeController : Controller
             return View("Index");
         }
         User? UserInDb = db.Users.FirstOrDefault(u=>u.Email == loginUser.Email);
+        if(UserInDb == null){
+            ModelState.AddModelError("Email", "Invalid Email/Password");    
+            return View("Index");
+        }
         PasswordHasher<LoginUser> hasher = new PasswordHasher<LoginUser>();
         var result = hasher.VerifyHashedPassword(loginUser, UserInDb.Password, loginUser.Password);
         if(result ==0){
